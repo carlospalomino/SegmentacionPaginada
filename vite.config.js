583a36c9e5605b1c59a60e9de0fae03e870fc880
Carlos Palomino <carlospalomino@outlook.com>
2026-05-16 23:58:39 -0500

    chore: Update vite base for SegmentacionPaginada and install gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SegmentacionSimple/', // Base URL para GitHub Pages
+  base: '/SegmentacionPaginada/', // Base URL para GitHub Pages
 })
